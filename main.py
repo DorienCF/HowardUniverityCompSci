26977ff3e401bb481981a16f4e8a3cab056982ee
Dorien <doriencasey@gmail.com>
2021-04-01 22:08:05 -0500

    01APR21

--- main.py
+++ main.py
@@ -16,22 +16,12 @@ for x in range(len(self.plaintext)):
 ## Making the class generate a file
 cypher_text.write("]")
 '''
-class Encryption(): # This Class Will control the encrption of the data
+class Encryption(): # This Class Will control the encrption of the data ## uncer construction
     def __init__(self):
         self.hour = datetime.utcnow().hour;self.minute = datetime.utcnow().minute;self.seccond = datetime.utcnow().second
         print(f'The current UTC time is: {self.hour}:{self.minute}:{self.seccond}\n')
         self.cypher_txt = open(f'{path}_encrypted_temp.json','r+')
-        '''
-        print(f'Would you like to begin data preperation Yes or No?')
-        DP_selection = input(">>")
-        if DP_selection.upper() == "Yes" or "Y":
-            pass   
-        elif DP_selection.upper() == "NO" or "N":
-            exit()
-        else:
-            print("Error, you entered an incorrect phrase...")
-        pass
-        '''
+    
         self.long = 5219283133417550597608921138394131714748003987111696388844721857021695621345566328693730284546120701185550350229748838662252951341253421746795 # long prime number used later
     def MESS_Encrypt(self):
         #hour  = datetime.utcnow().hour; minit = datetime.utcnow().minute; sec = datetime.utcnow().seccond
@@ -53,7 +43,7 @@ class Encryption(): # This Class Will control the encrption of the data
         pass
     pass
 
-class Decryption: # This class will control the decryption of the data
+class Decryption: # This class will control the decryption of the data || under construction
     def __init__(self):
         pass
     def RSA_Decrypt(self):
@@ -72,25 +62,28 @@ seccond = datetime.utcnow().second; minit = datetime.utcnow().minute; hour = dat
 s = open(path,'r'); plaintext = s.read()
 selection = input("would you like to 'Encrypt', 'Decrypt' or 'Exit'?\n>> ")
 
-if selection.upper() == 'DECRYPTION' or selection.upper() == "D":
+if selection.upper() == 'DECRYPT' or selection.upper() == "D":
     ## this will be the decryption pathway
     print(f'You entered {selection.upper()}\t Beginning Decryption...Nice')
     Decryption()
 
-elif selection.upper() == 'Encryption' or selection.upper() == 'E':
+elif selection.upper() == 'Encrypt' or selection.upper() == 'E':
     # This will be the Encryption pathway
-    package = {}
+    package = []
     print(f'You entered {selection.upper()}\nYou have chosen to encrypt ... Nice')
     with open(f'{path}_json_temp.json','w') as cypher_txt:
-        for x in range(len(plaintext)):
-         package += {
-            f'data_{x}': f'{ord(plaintext[x])}',
+        data = []
+        for i in range(len(plaintext)):
+            data.append(ord(plaintext[i]))
+            package = { 
+            f'data_':data ,
             'Hour':f'{hour}',
             'Minute':f'{minit}',
-            'Secconds':f'{seccond}',
-            
-            },
+            'Secconds':f'{seccond}'
+             },
         json.dump(package,cypher_txt)
+                            
+                            
     cypher_txt.close()
     Encryption()
 else:
